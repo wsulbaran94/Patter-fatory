5c0324086f230ffef4fee828737bfbec53a0d23a
wsulbaran28 <wilfredosulbaran94@gmail.com>
2023-11-13 14:37:00 -0500

    fix: create connection from instance factory class

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,7 +1,14 @@
 package Main;
 
+import Main.ConnectionConf.ConnectionInterface;
+
 public class Main {
     public static void main (String [] args) {
+        // Instances the factory and call the connection, put the driver for the configuration to db
+        FactoryConnection connF = new FactoryConnection();
+        ConnectionInterface conn = connF.getConnection("MYSQL");
 
+        conn.connect();
+        conn.disconnect();
     }
 }
